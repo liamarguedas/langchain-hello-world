4ec13e9e17355ceda7ec38e58be54264800e2097
liano <iliamftw2013@gmail.com>
2025-11-17 17:41:52 -0300

    add: langsmith

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 import os
 
 
@@ -6,7 +9,26 @@ def main():
 
     load_dotenv()
 
-    print(os.environ.get("OPENAI_API_KEY"))
+    information = """
+    Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
+l Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; his Canadian citizenship is congenital, his mother having been born there. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+    """
+
+    summary_template = """
+    give the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-4.1-mini")
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
